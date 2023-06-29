fedd569da49fd0c5c45b40959db8553003255f9b
MiguelMacias3M <miguelmacias3m@gmail.com>
2023-06-29 14:06:12 -0600

    Correccion de la ruta en el sistema principal

--- backend/sistema_iea/urls.py
+++ backend/sistema_iea/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('utna/', include('utna_module.urls')),
-    path('administration/', include('admin_module.urls'))
+    path('administration/', include('admin_module.urls')),
     path('utna_module/', include('utna_module.urls'))
 ]
+
