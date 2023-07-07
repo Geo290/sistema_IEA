2fbff3260cbb3945009275e650b1a50295d7c071
MiguelMacias3M <miguelmacias3m@gmail.com>
2023-07-07 12:39:33 -0600

    Normalizacion de rutas

--- backend/utna_module/urls.py
+++ backend/utna_module/urls.py
@@ -5,29 +5,30 @@ from utna_module import views
 
 
 router = routers.DefaultRouter()
-router.register(r'UTNA_Carreras', views.UTNA_Carrera_views, 'UTNA_Carreras')
-router.register(r'UTNA_Tasa', views.UTNA_Tasa_Cobertura_views, 'UTNA_Tasa')
-router.register(r'UTNA_Eficiencia_IRE', views.UTNA_Eficiencia_IRE_views, 'UTNA_Eficiencia_IRE')
-router.register(r'UTNA_Tasa_Puntaje', views.UTNA_Tasa_Puntaje_EGETSU_views, 'UTNA_Tasa_Puntaje')
-router.register(r'UTNA_Tasa_Puntaje_EGEL', views.UTNA_Tasa_Puntaje_EGEL_views, 'UTNA_Tasa_Puntaje_EGEL')
-router.register(r'UTNA_Tasa_Docentes', views.UTNA_Tasa_Docentes_views, 'UTNA_Tasa_Docentes')
-router.register(r'UTNA_Indice_PTC', views.UTNA_Indice_PTC_views, 'UTNA_Indice_PTC')
-router.register(r'UTNA_Tasa_PE_ASAT', views.UTNA_Tasa_PE_ASAT_views, 'UTNA_Tasa_PE_ASAT')
-router.register(r'UTNA_ISEg_TSU', views.UTNA_ISEg_TSU_views, 'UTNA_ISEg_TSU')
-router.register(r'UTNA_ISEg_L', views.UTNA_ISEg_L_views, 'UTNA_ISEg_L')
-router.register(r'UTNA_ISEm_TSU', views.UTNA_ISEm_TSU_views, 'UTNA_ISEm_TSU')
-router.register(r'UTNA_ISEm_L', views.UTNA_ISEm_L_views, 'UTNA_ISEm_L')
-router.register(r'UTNA_Indice_Servicios', views.UTNA_Indice_Servicios_views, 'UTNA_Indice_Servicios') 
-router.register(r'UTNA_Tasa_PE_Pertinencia', views.UTNA_Tasa_PE_Pertinencia_views, 'UTNA_Tasa_PE_Pertinencia')
-router.register(r'UTNA_Indice_Colocacion', views.UTNA_Indice_Colocacion_views, 'UTNA_Indice_Colocacion')
-router.register(r'UTNA_Tasa_Mov_Alumnos', views.UTNA_Tasa_Mov_Alumnos_views, 'UTNA_Tasa_Mov_Alumnos')
-router.register(r'UTNA_Tasa_Mov_Docentes', views.UTNA_Tasa_Mov_Docentes_views, 'UTNA_Tasa_Mov_Docentes')
-router.register(r'UTNA_Tasa_Acreditacion', views.UTNA_Tasa_Acreditacion_views, 'UTNA_Tasa_Acreditacion')
+router.register(r'carreras', views.UTNA_Carrera_views, 'UTNA_Carreras')
+router.register(r'tasa_cobertura', views.UTNA_Tasa_Cobertura_views, 'UTNA_Tasa')
+router.register(r'eficiencia_ire', views.UTNA_Eficiencia_IRE_views, 'UTNA_Eficiencia_IRE')
+router.register(r'tasa_puntaje', views.UTNA_Tasa_Puntaje_EGETSU_views, 'UTNA_Tasa_Puntaje')
+router.register(r'tasa_puntaje_egel', views.UTNA_Tasa_Puntaje_EGEL_views, 'UTNA_Tasa_Puntaje_EGEL')
+router.register(r'tasa_docentes', views.UTNA_Tasa_Docentes_views, 'UTNA_Tasa_Docentes')
+router.register(r'indice_ptc', views.UTNA_Indice_PTC_views, 'UTNA_Indice_PTC')
+router.register(r'tasa_pe_asat', views.UTNA_Tasa_PE_ASAT_views, 'UTNA_Tasa_PE_ASAT')
+router.register(r'iseg_tsu', views.UTNA_ISEg_TSU_views, 'UTNA_ISEg_TSU')
+router.register(r'iseg_l', views.UTNA_ISEg_L_views, 'UTNA_ISEg_L')
+router.register(r'isem_tsu', views.UTNA_ISEm_TSU_views, 'UTNA_ISEm_TSU')
+router.register(r'isem_l', views.UTNA_ISEm_L_views, 'UTNA_ISEm_L')
+router.register(r'indice_servicios', views.UTNA_Indice_Servicios_views, 'UTNA_Indice_Servicios') 
+router.register(r'tasa_pe_eertinencia', views.UTNA_Tasa_PE_Pertinencia_views, 'UTNA_Tasa_PE_Pertinencia')
+router.register(r'indice_colocacion', views.UTNA_Indice_Colocacion_views, 'UTNA_Indice_Colocacion')
+router.register(r'tasa_mov_alumnos', views.UTNA_Tasa_Mov_Alumnos_views, 'UTNA_Tasa_Mov_Alumnos')
+router.register(r'tasa_mov_docentes', views.UTNA_Tasa_Mov_Docentes_views, 'UTNA_Tasa_Mov_Docentes')
+router.register(r'tasa_acreditacion', views.UTNA_Tasa_Acreditacion_views, 'UTNA_Tasa_Acreditacion')
 
 
 
 urlpatterns = [
-  path('api/v1/utna_module/', include(router.urls)),
+  #http://127.0.0.1:8000/utna_module/api/utna_module/UTNA_Carreras/
+  path('api/', include(router.urls)),
   path('docs/', include_docs_urls(title='Documentacion del modulo UTNA')) # Documentation
 ]
 
