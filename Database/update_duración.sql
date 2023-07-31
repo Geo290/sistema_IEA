e3d55cd90816b169f690b4ca00315c835d9b8828
Flor_Zamorano <108295543+FlorZ5@users.noreply.github.com>
2023-07-31 15:29:31 -0600

    Delete update_duración.sql

--- Database/update_duración.sql
+++
@@ -1,4 +0,0 @@
-select*from utna_carreras
-update utna_carreras set duración=6 where modalidad='Escolarizado' and nivel='TSU';
-update utna_carreras set duración=9 where modalidad='Despresurizado' and nivel='TSU';
-update utna_carreras set duración=5 where modalidad='Escolarizado' and nivel='Licenciatura' or nivel='Ingeniería';
