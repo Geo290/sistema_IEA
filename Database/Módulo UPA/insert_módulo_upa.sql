c4a3938d22fb8de2a077e1dc44adc83823b4a42d
Flor_Zamorano <108295543+FlorZ5@users.noreply.github.com>
2023-07-31 16:34:57 -0600

    Módulo UPA
    Insert y query

---
+++ Database/Módulo UPA/insert_módulo_upa.sql
@@ -0,0 +1,391 @@
+--tabla upa_carreras
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Aeronáutica','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Electrónica','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Energía','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Sistemas Computacionales','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Sistemas Estratégicos de Información','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Industrial','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Mecánica Automotriz','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Mecatrónica','Escolarizado','Ingeniería',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Negocios Internacionales','Escolarizado','Licenciatura',10);
+insert into upa_carreras (universidad, nombre, modalidad, nivel, duración) values
+(6,'Negocios y Administración','Escolarizado','Licenciatura',10);
+
+--tabla upa_indice_colocacion
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (10,2018,62,55,88.7);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (9,2018,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (8,2018,109,95,87.2);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (7,2018,75,68,90.7);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (6,2018,88,78,88.6);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (5,2018,38,35,92.1);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (4,2018,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (3,2018,28,25,89.3);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (2,2018,14,14,100);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (1,2018,0,0,0);
+
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (10,2019,101,91,90.1);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (9,2019,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (8,2019,115,101,87.8);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (7,2019,77,65,84.4);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (6,2019,75,68,90.7);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (5,2019,43,38,88.4);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (4,2019,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (3,2019,30,27,90.0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (2,2019,26,23,88.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (1,2019,0,0,0);
+
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (10,2020,91,87,96.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (9,2020,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (8,2020,101,92,91.1);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (7,2020,73,68,93.2);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (6,2020,98,89,90.8);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (5,2020,44,30,68.2);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (4,2020,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (3,2020,23,20,87.0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (2,2020,26,25,69.2);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (1,2020,0,0,0);
+
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (10,2021,83,71,85.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (9,2021,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (8,2021,81,76,93.8);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (7,2021,76,70,92.1);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (6,2021,80,70,87.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (5,2021,32,25,78.1);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (4,2021,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (3,2021,29,26,89.7);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (2,2021,32,28,87.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (1,2021,0,0,0);
+
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (10,2022,70,60,85.7);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (9,2022,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (8,2022,83,74,89.2);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (7,2022,40,35,87.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (6,2022,88,81,92.0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (5,2022,37,27,73.0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (4,2022,0,0,0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (3,2022,22,21,95.5);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (2,2022,25,21,84.0);
+INSERT INTO upa_indice_colocacion(
+	carrera, periodo, egresados, colocados, indice_colocacion)
+	VALUES (1,2022,0,0,0);
+
+--tabla upa_tasa_mov_alumnos
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (10,2018,473,6,'NA','Canadá, España, Italia, Suiza',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (9,2018,0,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (8,2018,531,3,'NA','Chile, España',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (7,2018,433,4,'NA','Colombia, España',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (6,2018,458,6,'NA','Alemania, España, Francia, Italia',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (5,2018,225,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (4,2018,0,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (3,2018,190,1,'NA','Holanda',0);	
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (2,2018,187,3,'NA','Bélgica, Italia y Taiwán',0);		
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (1,2018,0,0,'NA','NA',0);	
+	
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (10,2021,212,13,'NA','Colombia, Francia, Turquía, Estados Unidos',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (9,2021,294,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (8,2021,480,5,'NA','Canadá, España, Rusia, Suiza',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (7,2021,380,6,'NA','España',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (6,2021,477,5,'NA','Francia,Estados Unidos',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (5,2021,95,1,'NA','Canadá',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (4,2021,126,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (3,2021,162,11,'NA','Epaña, Holanda',0);	
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (2,2021,172,2,'NA','Italia, Estados Unidos',0);		
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (1,2021,77,1,'NA','Emiratos Árabes Unidos',0);
+
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (10,2022,120,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (9,2022,405,2,'NA','España',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (8,2022,501,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (7,2022,374,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (6,2022,437,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (5,2022,53,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (4,2022,207,0,'NA','NA',0);
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (3,2022,137,1,'NA','Canadá',0);	
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (2,2022,151,0,'NA','NA',0);		
+INSERT INTO upa_tasa_mov_alumnos(
+	carrera, periodo, matricula, no_alum_viajaron, estados, paises, tasa_movilidad)
+	VALUES (1,2022,120,0,'NA','NA',0);
+
+
+--tabla upa_iseg
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (10,2022,0,0,0,0,8.4,8.2,0,0,0,8.9,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (9,2022,0,0,0,0,0,0,0,0,0,0,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (8,2022,0,0,0,0,9.5,9.5,0,0,0,9.6,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (7,2022,0,0,0,0,9.2,9.2,0,0,0,9.0,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (6,2022,0,0,0,0,8.9,9.1,0,0,0,8.5,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (5,2022,0,0,0,0,10.0,10.0,0,0,0,10.0,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (4,2022,0,0,0,0,0,0,0,0,0,0,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (3,2022,0,0,0,0,8.3,8.0,0,0,0,8.0,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (2,2022,0,0,0,0,9.3,9.1,0,0,0,7.7,0);
+INSERT INTO upa_iseg(
+	carrera, periodo, bolsa_trabajo, op_estadia, trabaja_lugar_estadia, experiencia_practica, laboratorios_talleres, 
+	infraestructura, dominio_prob_lab, conocimiento_prof, calificaion_mea5, op_preparacion, indice_satisfaccion)
+	VALUES (1,2022,0,0,0,0,0,0,0,0,0,0,0);
+
+--tabla upa_isem
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (10,2022,9.5,9.4,9.6,9.6,9.4,9.7,9.7,9.7,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (9,2022,0,0,0,0,0,0,0,0,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (8,2022,8.4,8.5,8.4,8.4,8.6,8.7,8.7,8.6,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (7,2022,9.2,9.1,9.1,9.0,9.5,9.8,9.5,9.4,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (6,2022,9.7,9.6,9.5,9.5,9.5,9.8,9.5,9.6,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (5,2022,9.5,9.5,9.6,9.7,9.8,9.7,9.8,9.6,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (4,2022,0,0,0,0,0,0,0,0,0);
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (3,2022,8.4,9.1,8.9,8.8,8.9,8.7,8.7,9.0,0);	
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (2,2022,8.8,8.9,9.2,8.8,9.1,9.3,8.8,9.5,0);	
+INSERT INTO upa_isem(
+	carrera, periodo, op_grado, creatividad, manejo_equipo, solucion_prob, op_trabajo, capacidad, cumple_req, 
+	calificaion_trabajo, indice_satisfaccion)
+	VALUES (1,2022,8.5,8.5,8.8,9.0,9.0,10.0,9.5,9.0,0);	
+
+--tabla upa_tasa_pe_petinencia
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (10,2023,'yes',default);
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (9,2023,'yes',default);
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (8,2023,'yes',default);
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (7,2023,'yes',default);
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (6,2023,'yes',default);
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (5,2023,'yes',default);
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (4,2023,'yes',default);	
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (3,2023,'yes',default);	
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (2,2023,'yes',default);	
+INSERT INTO upa_tasa_pe_pertinencia(
+	pe, periodo, presenta, fecha_elaboracion)
+	VALUES (1,2023,'yes',default);	
+
+
+select * from upa_carreras
