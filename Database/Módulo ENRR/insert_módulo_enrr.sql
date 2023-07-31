2bbd5f000a865bbffc867555b81ff9121b841406
Flor_Zamorano <108295543+FlorZ5@users.noreply.github.com>
2023-07-31 17:53:46 -0600

    Módulo ENRR
    Insert y query

---
+++ Database/Módulo ENRR/insert_módulo_enrr.sql
@@ -0,0 +1,4 @@
+insert into enrr_carreras (universidad, nombre, modalidad, nivel, duración) values
+(10,'Educación Física','Escolarizado','Licenciatura',8);
+insert into enrr_carreras (universidad, nombre, modalidad, nivel, duración) values
+(10,'Educación Primaria','Escolarizado','Licenciatura',8);
